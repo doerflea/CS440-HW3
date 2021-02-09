e549a9fb42db6bceb38994f0804584150745ba6c
Anneliese Doerfler <anneliesedoerfler@gmail.com>
2021-02-09 15:10:12 -0800

    IO bucket_array

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <unordered_map>
+#include <sstream>
+
+
+std::unordered_map<int,int> readBucketArray(){
+   std::unordered_map<int,int>mp;
+
+   //File with bucket array that maintains <h(key), bucket id>
+   std::ifstream bucket_array;
+   bucket_array.open("bucket_array.txt");
+   if(bucket_array.is_open()){
+      //read file into mp
+      std::string pair;
+      while(std::getline(bucket_array, pair)){
+	 std::stringstream ss(pair);
+	 std::string key, bucket_id;
+	 ss >> key >> bucket_id;
+	 mp[std::stoi(key)] = std::stoi(bucket_id);
+      }
+      bucket_array.close();
+   }
+   return mp;
+}
+
+int hash(int n){
+   //n num buckets
+   return 0 % n;
+}
 
 int main(int argc, char *argv[]){
    if(argc < 2){
@@ -10,16 +38,15 @@ int main(int argc, char *argv[]){
    //Look up mode, must include employee
    if(strcmp(argv[1],"L")  == 0 && argc == 3){
       std::cout << "lookup " << argv[2] << std::endl;
-
-      //file with bucket array that maintains <h(key), bucket id>
-      std::ifstream bucket_array;
-      bucket_array.open("bucket_array.txt");
-      if(bucket_array.is_open()){
-	 bucket_array.close();
-      }
-      else {
+      std::unordered_map<int,int> mp(readBucketArray());
+      if(mp.size() == 0){
 	 std::cout << "Employee not found" << std::endl;
       }
+      else{
+	 //get hash code
+	 //look up record file bucket id in mp
+      }
+      return 0;
    }
    //Creation mode
    else if(strcmp(argv[1],"C") == 0){
@@ -30,9 +57,16 @@ int main(int argc, char *argv[]){
       emp_file.open("Employees.csv");
 
       if(emp_file.is_open()){
+	 std::unordered_map<int,int>mp(readBucketArray());
 	 std::string tuple;
 	 while(std::getline(emp_file, tuple)){
-	    std::cout << tuple << std::endl;
+	    std::stringstream ss(tuple);
+	    std::string id;
+	    ss >> id;
+
+	    std::cout << "id: " << id << std::endl;
+	   
+	    //get hash code using id, read out to bucket, when buckets are 80% full add bucket, use bits ??
 	 }
 
 	 emp_file.close();
